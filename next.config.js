12a7a4af342bb10669273ed5e66862c04fb2860a
jacoboArcila <wagner.arcila.jacobo@gmail.com>
2023-08-25 22:34:35 -0500

    comentando configuracion de Nextconfig

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/* @type {import('next').NextConfig}
+const nextConfig = {
+	reactStrictMode: true,
+};
 
-module.exports = nextConfig;
+module.exports = nextConfig; */
 module.exports = {
 	images: {
 		domains: ["ecran.s3.amazonaws.com", "hydramovies.com"],
