f40226a03ed001e37f4473ce5a109cc1a5a032a9
jacoboArcila <wagner.arcila.jacobo@gmail.com>
2023-08-25 22:38:08 -0500

    i don't know

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
-/* @type {import('next').NextConfig}
+/** @type {import('next').NextConfig} */
+
 const nextConfig = {
 	reactStrictMode: true,
-};
-
-module.exports = nextConfig; */
-module.exports = {
 	images: {
 		domains: ["ecran.s3.amazonaws.com", "hydramovies.com"],
-		// Agrega el dominio de las imágenes aquí
 	},
+	swcMinify: true,
 };
+
+module.exports = nextConfig;
